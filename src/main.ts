b6467ac29b6c91f372db643c4b73276fe7a6f37a
plrtp <plrtp@mail.ru>
2025-06-09 21:11:45 +0300

    add: добавил разрешение по cors пока что со всех адресов

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+});
+
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 }
