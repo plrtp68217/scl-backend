369b7ab7b38f180d8d6e72e022c18d47c386e066
plrtp <plrtp@mail.ru>
2025-06-13 18:10:01 +0300

    update: api users - типизированы возвращаемые значения функций

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,23 +2,24 @@ import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './users.model';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get('/:userId')
-  get(@Param('userId') userId: number) {
+  get(@Param('userId') userId: number): Promise<User | null> {
     return this.usersService.getUser(userId);
   }
 
   @Post()
-  create(@Body() dto: CreateUserDto) {
+  create(@Body() dto: CreateUserDto): Promise<User> {
     return this.usersService.createUser(dto);
   }
 
   @Put()
-  update(@Body() dto: UpdateUserDto) {
+  update(@Body() dto: UpdateUserDto): Promise<User>{
     return this.usersService.updateUser(dto);
   }
 
