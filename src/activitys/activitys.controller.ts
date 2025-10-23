f446236ec6273d7d38119741d7835c18afa106ad
plrtp <plrtp@mail.ru>
2025-10-23 21:32:05 +0300

    update: acrtivitysService - дописана логика для обновления стрика входа в игру и получения награды, реализован контроллер

--- src/activitys/activitys.controller.ts
+++ src/activitys/activitys.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { ActivitysService } from './activitys.service';
+import { Activity } from './activitys.model';
+import { User } from 'src/users/users.model';
+import { CreateActivityDto } from './dto/create-activity.dto';
+import { UpdateActivityDto } from './dto/update-activity.dto';
+import { DeleteActivityDto } from './dto/delete-activity.dto';
 
 @Controller('activitys')
-export class ActivitysController {}
+export class ActivitysController {
+  constructor(private activitysService: ActivitysService) {}
+
+  @Post('create')
+  createActivity(@Body() dto: CreateActivityDto): Promise<Activity>  {
+    return this.activitysService.createActivity(dto);
+  }
+  
+  @Post('update')
+  updateActivity(@Body() dto: UpdateActivityDto): Promise<Activity>  {
+    return this.activitysService.updateActivity(dto);
+  }
+
+  @Post('delete')
+  deleteActivity(@Body() dto: DeleteActivityDto): Promise<boolean>  {
+    return this.activitysService.deleteActivity(dto);
+  }
+
+  @Get(':userId')
+  getActivityWithUpdatedStreak(@Param('userId') userId: number): Promise<Activity> {
+    return this.activitysService.getActivityWithUpdatedStreak(userId);
+  }
+
+  @Get('reward/:userId')
+  getReward(@Param('userId') userId: number): Promise<User | null> {
+    return this.activitysService.getReward(userId);
+  }
+}
--- src/activitys/activitys.service.ts
+++ src/activitys/activitys.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Activity } from './activitys.model';
 import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/users.model';
 
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
 import { DeleteActivityDto } from './dto/delete-activity.dto';
+import { DateService } from 'src/common/date.service';
 
 @Injectable()
 export class ActivitysService {
+  private readonly MAX_STREAK_VALUE = 7;
+
   constructor(
     @InjectModel(Activity)
     private activityModel: typeof Activity,
     private usersService: UsersService,
+    private dateService: DateService
   ) {}
   
+  // CRUD
   async createActivity(dto: CreateActivityDto) {
     const activity = await this.activityModel.create(dto);
     return activity;
@@ -26,12 +32,17 @@ export class ActivitysService {
         userId
       }
     });
-    return activity;
+
+    if (!activity) {
+      throw new Error('activity not found'); 
+    }
+
+    return activity
   }
 
   async updateActivity(dto: UpdateActivityDto) {
     const activity = await this.getActivity(dto.userId);
-    await activity?.update(dto);
+    await activity.update(dto);
     return activity;
   }
 
@@ -39,6 +50,70 @@ export class ActivitysService {
     const activity = await this.getActivity(dto.userId);
     await activity?.destroy();
     return true;
+  }
+
+
+  // ИСПОЛЬЗУЕТСЯ ДЛЯ ПОЛУЧЕНИЯ АКТИВНОСТИ ПРИ ВХОДЕ В EARN
+  async getActivityWithUpdatedStreak(userId: number): Promise<Activity> {
+    let activity = await this.activityModel.findOne({
+      where: {
+        userId
+      }
+    });
+
+    if(!activity) {
+      const createActivityDto = {userId};
+      activity = await this.createActivity(createActivityDto)
+    }
+
+    if (this.dateService.isToday(activity.lastEntry)) {
+      return activity;
+    }
+
+    this.updateActivityStreak(activity);
+    await activity.save();
+
+    return activity;
+  }
+
+  // ВЫДАЕМ НАГРАДУ ПОЛЬЗОВАТЕЛЮ
+  async getReward(userId: number): Promise<User | null> {
+    const activity = await this.getActivity(userId);
+
+    if (!activity) {
+      return null;
+    }
+
+    if (activity.isRewarded == true) {
+      return null;
+    }
+
+    const user = await this.usersService.getUser(userId);
+
+    if (!user) {
+      return null;
+    }
+
+    activity.isRewarded = true;
+    await activity.save();
+
+    user.balance += activity.reward * activity.streak
+    await this.usersService.updateUser(user);
+
+    return user;
+  }
+
+  private updateActivityStreak(activity: Activity): void {
+    const isNextDayToday = this.dateService.isNextDayToday(activity.lastEntry);
+    
+    activity.streak = isNextDayToday ? activity.streak + 1 : 1;
+    
+    if (activity.streak > this.MAX_STREAK_VALUE) {
+      activity.streak = 1;
+    }
+
+    activity.lastEntry = new Date();
+    activity.isRewarded = false;
   }
 
 }
