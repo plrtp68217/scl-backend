882ef0e1251435596b2f0ea3a44fc07915ee8f92
plrtp <plrtp@mail.ru>
2025-10-23 21:30:29 +0300

    add: dateService доавблен в модуль по работе с активностями

--- src/activitys/activitys.module.ts
+++ src/activitys/activitys.module.ts
@@ -4,10 +4,11 @@ import { ActivitysController } from './activitys.controller';
 import { ActivitysService } from './activitys.service';
 import { Activity } from './activitys.model';
 import { UsersModule } from 'src/users/users.module';
+import { DateService } from 'src/common/date.service';
 
 @Module({
   controllers: [ActivitysController],
-  providers: [ActivitysService],
+  providers: [ActivitysService, DateService],
   imports: [
     SequelizeModule.forFeature([Activity]),
     UsersModule,
